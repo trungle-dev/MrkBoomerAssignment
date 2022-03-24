eaf1519e8893ca92a89ace5e3d48744af44b5b09
Trung Le <trungle.cs@gmail.com>
2022-03-24 21:41:51 +0700

    add ENV FILE PATH INPUT

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative './models/item'
 require_relative './services/printer'
 require_relative './services/reader'
 
-sample_path = './samples/sample_2.txt'
+sample_path = ENV['SAMPLE_INPUT_PATH'] || './samples/sample_2.txt'
 
 items_map = Reader.new(sample_path).call
 
