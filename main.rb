eeb57182b775673d48394d51bc0b6cc0fceebd1a
Trung Le <trungle.cs@gmail.com>
2022-03-24 21:21:00 +0700

    implement printer service and integrate execution

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
-puts 'Hello world'
+require_relative './models/item'
+require_relative './services/printer'
+require_relative './services/reader'
+
+sample_path = './samples/sample_2.txt'
+
+items_map = Reader.new(sample_path).call
+
+Printer.new(items_map).call
---
+++ services/printer.rb
@@ -0,0 +1,30 @@
+class Printer
+  INDENTED_WIDTH = 2
+
+  def initialize(items_map)
+    @items_map = items_map
+  end
+
+  def call
+    print_list(@items_map['nil'], 0)
+  end
+
+  private
+
+  def print_list(items, level)
+    items.each do |item|
+      print_node(item, @items_map[item.id], level)
+    end
+  end
+
+  def print_node(node, childs, level)
+    space = ' ' * INDENTED_WIDTH * level
+
+    puts "#{space}#{node}"
+
+    return if childs&.empty?
+
+    level += 1
+    print_list(childs, level)
+  end
+end
